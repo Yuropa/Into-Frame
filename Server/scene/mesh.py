839a8685a254aceaaa34866d810f921e7a60b389
Josh Ford <joshua.ford@polytechnique.edu>
2026-04-23 13:02:35 +0200

    Update scaling

--- Server/scene/mesh.py
+++ Server/scene/mesh.py
@@ -1,4 +1,5 @@
 import trimesh
+import numpy as np
 
 class Mesh:
     def __init__(self, mesh: trimesh.Trimesh) -> None:
@@ -12,6 +13,19 @@ class Mesh:
     @property
     def face_count(self) -> int:
         return len(self.mesh.faces)
+
+    @property
+    def extents(self) -> np.ndarray:
+        return self.mesh.extents
+
+    @property
+    def center(self) -> np.ndarray:
+        return self.mesh.centroid
+    
+    def fit_to_box(self, width: float, height: float) -> None:
+        scale = min(width / self.extents[0], height / self.extents[1])
+        self.mesh.apply_scale(scale)
+        self.mesh.apply_translation(-self.mesh.centroid)
 
     def save(self, path):
         self.mesh.export(str(path), include_normals=True)
