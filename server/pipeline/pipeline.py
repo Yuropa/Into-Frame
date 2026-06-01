2bf04b36adf8b3d8ff82da5daf3b3c8049ffaa44
Josh Ford <me@jford.link>
2026-06-01 11:31:10 +0200

    Fix some caching

--- server/pipeline/pipeline.py
+++ server/pipeline/pipeline.py
@@ -268,7 +268,7 @@ class Pipeline:
         if progress_queue is not None:
             progress_queue.put({"step": self.current_step, "percent": self.current_step_index / float(len(self.stages))})
 
-    def _run_stage(self, stage: PipelineStage, context: PipelineContext, progress_queue: Optional[queue.SimpleQueue], monitor, progress, task, force_run: bool = False) -> bool:
+    def _run_stage(self, stage: PipelineStage, context: PipelineContext, progress_queue: Optional[queue.SimpleQueue], monitor, progress, task) -> bool:
         output_root, temp_root = self._create_output_directories()
         stage.set_output(output_root, temp_root)
 
@@ -281,7 +281,7 @@ class Pipeline:
                 self._post_progress(progress_queue)
 
                 context.push_stage(stage.name)
-                if force_run or not stage.has_expected_output(context):
+                if not stage.has_expected_output(context):
                     context = stage.run(context)
                     stage.log_memory_usage()
                     stage.clean_up()
@@ -339,19 +339,15 @@ class Pipeline:
             ) as progress:
                 task = progress.add_task("Processing...", total=len(self.stages))
 
-                force_run = False
                 for stage in self.stages:
-                    ran = self._run_stage(
+                    self._run_stage(
                         stage=stage,
                         context=context,
                         progress_queue=progress_queue,
                         monitor=monitor,
                         progress=progress,
                         task=task,
-                        force_run=force_run,
                     )
-                    if ran:
-                        force_run = True
 
         self._save_context(context)
         monitor.print_summary()
